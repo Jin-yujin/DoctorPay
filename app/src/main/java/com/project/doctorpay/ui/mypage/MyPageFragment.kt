2de33a87b0649e48393b6c6c8d3c1dfa8f85e1db
Jin-yujin <yjinj0383@naver.com>
2024-09-30 20:31:56 +0900

    마이페이지 ui

--- app/src/main/java/com/project/doctorpay/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/project/doctorpay/ui/mypage/MyPageFragment.kt
@@ -1,8 +1,24 @@
 package com.project.doctorpay.ui.mypage
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.project.doctorpay.R
 
 class MyPageFragment : Fragment(){
 
+    private lateinit var tvVersion: TextView
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_mypage, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvVersion = view.findViewById(R.id.tvVersion)
+    }
 }
